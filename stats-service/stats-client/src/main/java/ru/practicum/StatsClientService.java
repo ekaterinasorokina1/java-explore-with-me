08533d476b33f1f2d3e178ea16dba6d93fe4d2d0
seokatrin <seokatrin1@yandex.ru>
2025-12-01 22:00:44 +0500

    feat(stat_svc): реализация сервиса статистики

---
+++ stats-service/stats-client/src/main/java/ru/practicum/StatsClientService.java
@@ -0,0 +1,12 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsClientService {
+    public static void main(String[] args) {
+        SpringApplication.run(StatsClientService.class, args);
+    }
+
+}
